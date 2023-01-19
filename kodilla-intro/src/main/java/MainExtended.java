cd4400e816610381e09d6c0dc27592827196195d
Anna Aleksandrowicz <anna.aleksandrowicz09@gmail.com>
2023-01-19 18:58:48 +0100

    Zmienne

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,19 @@
+public class MainExtended {
+
+    public static void main(String[] args)
+    {
+        String name = "Anna";
+        String surname = "Aleksandrowicz";
+        char firstLetter = 'A';
+        int age = 3;
+        boolean ifTrue = true;
+        double average = 8.5;
+
+        System.out.println(name);
+        System.out.println(surname);
+        System.out.println(firstLetter);
+        System.out.println(age);
+        System.out.println(ifTrue);
+        System.out.println(average);
+    }
+}
