1e7d9ecd25d2fb4969a96cc90bdd0576b15bb751
Anna Aleksandrowicz <anna.aleksandrowicz09@gmail.com>
2023-03-16 19:50:32 +0100

    Collections Advanced - homework   Immutable classes

---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/homework/TaskApplication.java
@@ -0,0 +1,7 @@
+package com.kodilla.collections.adv.immutable.homework;
+
+public class TaskApplication {
+
+    Task task = new Task("title", 21);
+    TaskRecord taskRecord = new TaskRecord("title", 21, 23);
+}
