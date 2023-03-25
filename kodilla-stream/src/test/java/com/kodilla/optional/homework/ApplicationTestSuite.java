ffef0c3f1b24ecbc6982ab38ef556007f2fc3ce2
Anna Aleksandrowicz <anna.aleksandrowicz09@gmail.com>
2023-03-25 20:10:16 +0100

    Homework From Streams List of Students and Average of Posts v.2

--- kodilla-stream/src/test/java/com/kodilla/optional/homework/ApplicationTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/optional/homework/ApplicationTestSuite.java
@@ -12,27 +12,23 @@ public class ApplicationTestSuite {
 
     @Test
     public void testGetTeacherNameForValidPromoter() {
-        // arrange
+
         Student janKowalski = new Student("Jan Kowalski", new Teacher("Edward Paczes"));
 
-        // act
         Optional<Teacher> optionalTeacher = Optional.ofNullable(janKowalski.getTeacher());
         String teacherName = optionalTeacher.isPresent() ? optionalTeacher.get().getName() : "<undefined>";
 
-        // assert
         assertEquals("Edward Paczes", teacherName);
     }
 
     @Test
     public void testGetTeacherNameForInvalidPromoter() {
-        // arrange
+
         Student ewaKowalska = new Student("Ewa Kowalska", null);
 
-        // act
         Optional<Teacher> optionalTeacher = Optional.ofNullable(ewaKowalska.getTeacher());
         String teacherName = optionalTeacher.isPresent() ? optionalTeacher.get().getName() : "<undefined>";
 
-        // assert
         assertEquals("<undefined>", teacherName);
     }
 
